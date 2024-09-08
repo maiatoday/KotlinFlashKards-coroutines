b9a38bce9e81ece65dd4f56278d91922f89e5864
Maia Grotepass <maiatoday@gmail.com>
2024-09-08 10:58:21 +0200

    Structured concurrency 6

--- Coroutines/Structured concurrency/6/src/Task.kt
+++ Coroutines/Structured concurrency/6/src/Task.kt
@@ -1,1 +1,37 @@
-// type your solution here
+import kotlinx.coroutines.*
+import kotlin.time.Duration.Companion.seconds
+
+@OptIn(ExperimentalCoroutinesApi::class)
+fun main() = runBlocking {
+    // Creating a parent job
+    val parentJob = Job()
+
+    // Launching a child coroutine
+    val childJob = launch(parentJob) {
+        delay(1.seconds)
+        println("Child coroutine completed")
+    }
+
+    // Launching a deferred child coroutine
+    val childJobDeferred = async(parentJob) {
+        delay(1.seconds)
+        println("Child coroutine completed (deferred)")
+        "Deferred"
+    }
+
+    // Displaying relationship
+    println("Parent job: $parentJob")
+    println("Child 1 job: $childJob")
+    println("Child 2 job (deferred): $childJobDeferred")
+
+    // Showing children
+    println("Children of parent job: ${parentJob.children.toList()}")
+
+    // Showing parent
+    println("Parent of child 1 job: ${childJob.parent}")
+    println("Parent of child 2 job (deferred): ${childJobDeferred.parent}")
+
+    // Waiting for child completion
+    childJobDeferred.await()
+    childJob.join()
+}
