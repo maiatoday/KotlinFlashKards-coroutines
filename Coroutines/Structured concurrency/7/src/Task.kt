204dcbf2925d5c79c04fd60dfdc87df161b098bb
Maia Grotepass <maiatoday@gmail.com>
2024-09-08 14:49:34 +0200

    Structured concurrency 7

--- Coroutines/Structured concurrency/7/src/Task.kt
+++ Coroutines/Structured concurrency/7/src/Task.kt
@@ -1,1 +1,17 @@
-// type your solution here
+import kotlinx.coroutines.*
+import kotlin.time.Duration.Companion.milliseconds
+
+fun main() = runBlocking {
+    val job = launch {
+        repeat(100) { i ->
+            delay(100.milliseconds)
+            println("Coroutine is working on iteration $i")
+        }
+    }
+
+    delay(500.milliseconds) // Let the coroutine run for a bit
+    println("Main thread cancels the coroutine")
+    job.cancelAndJoin() // Cancel the coroutine and wait for its completion
+    println("Coroutine has been cancelled")
+    check(job.isCancelled)
+}
--- Coroutines/Structured concurrency/7/task.md
+++ Coroutines/Structured concurrency/7/task.md
@@ -3,5 +3,5 @@ Question:
 **How do you stop your coroutine manually before it completes?**
 
 <div class="hint">
-  `cancel()`
+  `cancel()` or 'cancelAndJoin()' to wait for the cancellation to complete
 </div>
