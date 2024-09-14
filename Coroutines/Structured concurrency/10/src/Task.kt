ebc7db2ff6a56e60b6f5ac51ddc7916617fb1061
Maia Grotepass <maiatoday@gmail.com>
2024-09-14 14:46:16 +0200

    Structured concurrency 10

--- Coroutines/Structured concurrency/10/src/Task.kt
+++ Coroutines/Structured concurrency/10/src/Task.kt
@@ -1,1 +1,25 @@
-// type your solution here
+import kotlinx.coroutines.*
+import kotlin.time.Duration.Companion.milliseconds
+
+fun main() = runBlocking {
+    val job = launch {
+        while(isActive) {
+            val x = doSomething()
+            println(x)
+        }
+    }
+    delay(200.milliseconds)
+    job.cancel()
+    println("\nDone")
+
+}
+
+suspend fun doSomething():Int {
+    println("Do something")
+    var counter = 0
+    repeat(5) {
+        counter++
+        delay(10.milliseconds) // suspend function so it can be cancelled
+    }
+    return counter
+}
--- Coroutines/Structured concurrency/11/task.md
+++ Coroutines/Structured concurrency/11/task.md
@@ -1,6 +1,6 @@
 Question:
 
-**Which function can you call to throw a CancelationExeption to allow a coroutine with a tight loop to break out of the loop and be cancelled cooperatively?**
+**Which function can you call to throw a CancellationException to allow a coroutine with a tight loop to break out of the loop and be cancelled cooperatively?**
 
 <div class="hint">
   `ensureActive()`
