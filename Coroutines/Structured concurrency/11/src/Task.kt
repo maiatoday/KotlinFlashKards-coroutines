638db75e0acc2a43f092c23ef953bd0d80a04eb7
Maia Grotepass <maiatoday@gmail.com>
2024-09-14 14:54:31 +0200

    Structured concurrency 11 and 12

--- Coroutines/Structured concurrency/11/src/Task.kt
+++ Coroutines/Structured concurrency/11/src/Task.kt
@@ -1,1 +1,26 @@
-// type your solution here
+import kotlinx.coroutines.*
+import kotlin.time.Duration.Companion.milliseconds
+
+fun main() = runBlocking {
+    val job = launch {
+        while(true) {
+            val x = doSomething()
+            println(x)
+            ensureActive()
+        }
+    }
+    delay(200.milliseconds)
+    job.cancel()
+    println("\nDone")
+
+}
+
+suspend fun doSomething():Int {
+    println("Do something")
+    var counter = 0
+    repeat(5) {
+        counter++
+        delay(10.milliseconds) // suspend function so it can be cancelled
+    }
+    return counter
+}
--- Coroutines/Structured concurrency/12/src/Task.kt
+++ Coroutines/Structured concurrency/12/src/Task.kt
@@ -1,1 +1,27 @@
-// type your solution here
+import kotlinx.coroutines.*
+import kotlin.time.Duration.Companion.milliseconds
+
+fun main() = runBlocking {
+    val job = launch {
+        while(true) {
+            val x = doSomething()
+            println(x)
+            delay(20.milliseconds)
+            ensureActive()
+        }
+    }
+    delay(100.milliseconds)
+    job.cancel()
+    println("\nDone")
+
+}
+
+suspend fun doSomething():Int {
+    println("Do something")
+    var counter = 0
+    repeat(1000) {
+        counter++
+        yield() // We need something to break out the tight loop
+    }
+    return counter
+}
